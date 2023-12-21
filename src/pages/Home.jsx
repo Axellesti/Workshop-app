2df0e97bc42ec4cea9f099aa44f957955b238043
Axellesti <sartori.axelle18@gmail.com>
2023-12-21 10:21:23 +0100

    on ajoute un useEffect météo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
 
+const getWeatherOfTheDay = () => {
+  return "sunny";
+};
 
 function Home() {
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    const weatherOfTheDay = getWeatherOfTheDay();
+
+    setWeather(weatherOfTheDay);
+  }, []);
+
   return (
-    <h1>Hello from Home</h1>
-  )
+    <>
+      <h1>Hello from Home</h1>
+
+      {weather != null && <p>Today is a {weather} day</p>}
+    </>
+  );
 }
 
-export default Home
+export default Home;
